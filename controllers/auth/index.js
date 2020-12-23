91b7b0c496c0520139ff1ab9e2814d069aeb64e7
emmayusufu <kimaswaemma36@gmail.com>
2020-12-23 23:40:00 +0300

    chan

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -197,32 +197,54 @@ exports.setupWorkManProfile = (req, res) => {
     profession,
   } = req.body;
 
-  const dpImage = req.files.find((e) => e.fieldname == "dpImage");
-  const idBack = req.files.find((e) => e.fieldname == "idBack");
-  const idFront = req.files.find((e) => e.fieldname == "idFront");
+  const dpImage = toBase64(req.files.find((e) => e.fieldname == "dpImage"));
+  const idBack = toBase64(req.files.find((e) => e.fieldname == "idBack"));
+  const idFront = toBase64(req.files.find((e) => e.fieldname == "idFront"));
 
-  console.log(toBase64(req.files.find((e) => e.fieldname == "dpImage")));
-
-  //   UserModal.findOne({ where: { id: id } })
-  //     .then((user) => {
-  //       if (user) {
-  //         user.firstName = firstName;
-  //         user.lastName = lastName;
-  //         user.areaOfOperation = areaOfOperation;
-  //         user.dob = dob;
-  //         user.qualification = qualification;
-  //         user.extraSkills = extraSkills;
-  //         user.nin = nin;
-  //         user.profession = profession;
-  //         user
-  //           .save()
-  //           .then(() => {})
-  //           .catch((e) => console.log(e));
-  //       } else {
-  //         res.json();
-  //       }
-  //     })
-  //     .catch((e) => console.log(`caught error ${e}`));
+  UserModal.findOne({ where: { id: id } })
+    .then((user) => {
+      if (user) {
+        user.firstName = firstName;
+        user.lastName = lastName;
+        user.areaOfOperation = areaOfOperation;
+        user.dob = dob;
+        user.qualification = qualification;
+        user.extraSkills = extraSkills;
+        user.nin = nin;
+        user.profession = profession;
+        user
+          .save()
+          .then(() => {
+            cloudinary.uploader
+              .upload(dpImage, function (error, result) {
+                user.dpImage = result.url;
+                user.save();
+              })
+              .then(() => {
+                cloudinary.uploader
+                  .upload(idFront, function (error, result) {
+                    user.idFront = result.url;
+                    user.save();
+                  })
+                  .then(() => {
+                    cloudinary.uploader.upload(
+                      idBack,
+                      function (error, result) {
+                        user.idBack = result.url;
+                        user.save().then(() => {
+                          res.json({ message: "success" });
+                        });
+                      }
+                    );
+                  });
+              });
+          })
+          .catch((e) => console.log(e));
+      } else {
+        res.json();
+      }
+    })
+    .catch((e) => console.log(`caught error ${e}`));
 };
 
 exports.users = (req, res) => {
